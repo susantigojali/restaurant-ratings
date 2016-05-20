f444d01d364fe1cc25ed6b4d1cb576eb96186cb3
susantigojali <susantigojali@hotmail.com>
2016-05-20 13:47:20 +0700

    Add aspect aggregation model

---
+++ src/Model/AspectAggregation.java
@@ -0,0 +1,143 @@
+package Model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author susanti_2
+ */
+public class AspectAggregation {
+    
+    private static final String DICTIONARY_FILENAME = "dict/category_aspect_dict.txt";
+
+    public static final String OTHER_CATEGORY = "other";
+    
+    private static final LinkedHashMap<String, ArrayList<String>> aspectCategoryDicts = new LinkedHashMap<>();
+    
+    private static void intDict() throws FileNotFoundException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(DICTIONARY_FILENAME));
+        String line;
+
+        try {
+            String categoryName = "";
+            ArrayList<String> categories = new ArrayList<>();
+            while ((line = fileReader.readLine()) != null) {
+                if (! line.equals("")) {
+                    if (line.startsWith("#")) {
+                        categoryName = line.substring(1);
+                        categories = new ArrayList<>();
+                    } else {
+                        categories.add(line);
+                    } 
+                } else {
+                    aspectCategoryDicts.put(categoryName, categories);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Translator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * aggregate aspect sentiment based on dictionary
+     * @param aspectSentiments list of aspect sentiment to be aggregated
+     * @return aggregation of aspect sentiment
+     * @throws FileNotFoundException dictionary not found
+     */
+    public static LinkedHashMap<String, ArrayList<AspectSentiment>> aggregation(ArrayList<AspectSentiment> aspectSentiments) throws FileNotFoundException {
+        LinkedHashMap<String, ArrayList<AspectSentiment>> aspectAggregations = new LinkedHashMap<>() ;
+        for (AspectSentiment as : aspectSentiments) {
+            String category = getCategory(as.getAspect());
+            System.out.println("category "+ as.getAspect() + " = " +category);
+            if (aspectAggregations.containsKey(category)) {
+                ArrayList<AspectSentiment> newAS = new ArrayList<>(aspectAggregations.get(category));
+                newAS.add(as);
+                aspectAggregations.put(category, newAS);
+            } else {
+                ArrayList<AspectSentiment> newAS = new ArrayList<>();
+                newAS.add(as);
+                aspectAggregations.put(category,newAS);
+            }
+        }
+        
+        return aspectAggregations;
+    }
+    
+    /**
+     * categorize aspect based on dictionary
+     * @param aspect aspect
+     * @return category of aspect
+     * @throws FileNotFoundException dictionary not found
+     */
+    public static String getCategory (String aspect) throws FileNotFoundException {
+        if (aspectCategoryDicts.isEmpty()) {
+            intDict();
+        }
+        
+        ArrayList<String> trans = Translator.getTranslation(aspect);
+        double max = Double.NEGATIVE_INFINITY;
+        String category = null;
+        boolean found = false;
+        
+        for (String categoryName : aspectCategoryDicts.keySet() ) {
+//            System.out.println("category name: "+ categoryName);
+            ArrayList<String> value = aspectCategoryDicts.get(categoryName);
+            
+            for (int i = 0 ; i < value.size() && !found; i++) {//String cat : value) {
+                String cat = value.get(i);
+//                System.out.println("cat: "+cat);
+                ///no translate found
+                if (trans == null) {
+                    found = true;
+                    category = OTHER_CATEGORY;
+                } else {
+                    for (int j = 0; j < trans.size() && !found; j++) {
+//                    System.out.println("trans " + j + " : "+ trans.get(j) + " "+ cat);
+                        if (cat.compareTo(trans.get(j)) == 0) {
+                            found = true;
+                            category = categoryName;
+                        } else {
+                            double jcn = Wordnet.jcn(trans.get(j), cat);
+                            if (jcn > max) {
+                                max = jcn;
+        //                        System.out.println("max : "+ jcn + " " + cat);
+                                category = categoryName;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return category;
+    }
+            
+
+    public static void main (String args[]) {
+        try {
+            ArrayList<AspectSentiment> aspectSentiments = new ArrayList<>();
+            aspectSentiments.add(new AspectSentiment("makanan", "enak", 1));
+            aspectSentiments.add(new AspectSentiment("harga", "murah", 1));
+            aspectSentiments.add(new AspectSentiment("makanan", "lezat", 1));
+            LinkedHashMap<String, ArrayList<AspectSentiment>> aggregation = aggregation(aspectSentiments);
+            
+            System.out.println(aggregation.size());
+            for (String key : aggregation.keySet()) {
+                ArrayList<AspectSentiment> value = aggregation.get(key);
+                System.out.println(key + "========= " );
+                for (int i = 0; i < value.size(); i++) {
+                    System.out.println(value.get(i).getAspect()+ " " + value.get(i).getSentiment());
+                }
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AspectAggregation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
