03a41088d8040ae20f143b0b2aa767e8fc3cb2e5
susantigojali <susantigojali@hotmail.com>
2016-06-09 21:49:19 +0700

    Modify rating

--- src/Model/Rating.java
+++ src/Model/Rating.java
@@ -7,14 +7,24 @@ import java.util.ArrayList;
  * @author susanti_2
  */
 public class Rating {
+
+    /**
+     * count the rating 
+     * @param aspectSentiments list of aspectsentiments
+     * @return rating 
+     */
     public static double getRating (ArrayList<AspectSentiment> aspectSentiments) {
         double sumPos = 0;
+        double sumNeg = 0;
         for (AspectSentiment as: aspectSentiments) {
             if (as.isPositive()) {
                 sumPos++;
             }
+            if (as.isNegative()) {
+                sumNeg++;
+            }
         }
         
-        return (double)(4 * (sumPos/aspectSentiments.size()))+ 1;
+        return (double)(4 * (sumPos/(sumNeg+sumPos)))+ 1;
     }
 }
